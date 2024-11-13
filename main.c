d80bdb36041f594578789855c814fc2acd03d27e
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-11-14 01:11:17 +0200

    added main.c + .gitignore + Makefile

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float add(float a, float b);
+float subtract(float a, float b);
+double multiply(float a, float b);
+float divide(float a, float b);
+
+int main(void)
+{
+	return 1;
+}
