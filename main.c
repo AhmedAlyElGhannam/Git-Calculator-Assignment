8f21f9d4db527d9756cf06c25844bc28030e2863
YasmeenYasser611 <yasmeenyasser611@gmail.com>
2024-11-14 10:14:43 +0200

    subtraction function

--- main.c
+++ main.c
@@ -8,5 +8,15 @@ float divide(float a, float b);
 
 int main(void)
 {
+	float num1, num2;
+	printf("please enter 2 numbers:");
+    scanf(" %f", &num1);	
+	scanf(" %f", &num2);
+	printf("the sub of the 2 numbers is =%f", subtract(num1 , num2));
 	return 1;
+}
+
+float subtract(float a, float b)
+{
+	return a-b ;
 }
