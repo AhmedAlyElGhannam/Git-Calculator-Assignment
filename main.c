60a70567e6d22307c031e6c15e9309b64f18e8d5
Mariam-Hossam-Saleh <mariam.hossam.keshk@gmail.com>
2024-11-14 11:33:06 +0200

    Multipy_Feature

--- main.c
+++ main.c
@@ -8,5 +8,16 @@ float divide(float a, float b);
 
 int main(void)
 {
+	float num1,num2;
+	printf("Please enter number1 : \n");
+	scanf(" %f",&num1);
+	printf("Please enter number2 : \n");
+	scanf(" %f",&num2);
+	printf("Multiply : \n %0.2f * %0.2f = %0.2lf",num1,num2,multiply(num1,num2));
 	return 1;
+}
+
+double multiply(float a, float b)
+{
+	return (a * b);
 }
