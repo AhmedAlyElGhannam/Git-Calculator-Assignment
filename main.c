a45bc202968682bdb5052c6b6a364856a361b88e
Ali-Elmansoury <ali_elmansoury2001@live.com>
2024-11-14 12:29:49 +0200

    Resolve conflicts between main and mod branch

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ float add(float a, float b);
 float subtract(float a, float b);
 double multiply(float a, float b);
 float divide(float a, float b);
+int Modulus(int a, int b);
 
 int main(void)
 {
@@ -14,7 +15,8 @@ int main(void)
     scanf(" %f", &num1);	
 	printf("num2=");
 	scanf(" %f", &num2);
-	printf("the sub of the 2 numbers is =%f", subtract(num1 , num2));
+	printf("the sub of the 2 numbers is =%f\n", subtract(num1 , num2));
+	printf("8 Mod 3 = %d\n", Modulus(8, 3));
 	return 1;
 }
 
@@ -31,9 +33,16 @@ float add(float a, float b)
 	return a + b;
 }
 
-
 float subtract(float a, float b)
 {
 	
 	return a-b ;
+}
+
+int Modulus(int a, int b)
+{
+	if (b == 0)
+		return 0;
+
+	return (a % b);
 }
