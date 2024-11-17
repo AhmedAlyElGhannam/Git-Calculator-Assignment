c2c5a666e9d41c9f959cd8c9e8d8b78819695f36
Mariam-Hossam-Saleh <mariam.hossam.keshk@gmail.com>
2024-11-17 18:16:02 +0200

    Redirecting The Mul_Feature Folder to Drivers folder

---
+++ Drivers/MulFeature/inc/Mul.h
@@ -0,0 +1,14 @@
+/*
+ *  FILE  :  Mul.h
+ *  Date   :  Nov 17, 2024
+ *  Author :  Mariam Hossam
+ */
+ 
+/****************    Header File Guard    ****************/
+#ifndef MUL_H
+#define MUL_H
+
+/***************    Functioon Prototype    ***************/
+double multiply(float a, float b);
+
+#endif // MUL_H 
---
+++ Drivers/MulFeature/src/Mul.c
@@ -0,0 +1,13 @@
+/*
+ *  FILEE  :  Mul.c
+ *  Date   :  Nov 17, 2024
+ *  Author :  Mariam Hossam
+ */
+ 
+#include "Mul.h"
+ 
+/***************    Function Implementation    ***************/
+double multiply(float a, float b)
+{
+	return (a * b);
+}
