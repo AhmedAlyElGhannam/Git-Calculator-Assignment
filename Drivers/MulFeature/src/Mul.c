cccbb4ca06c4428609970713f5737fded745d31b
Mariam-Hossam-Saleh <mariam.hossam.keshk@gmail.com>
2024-11-24 15:13:56 +0200

    updated Mul.c (removing #include mul.h & adding macro)

--- Drivers/MulFeature/src/Mul.c
+++ Drivers/MulFeature/src/Mul.c
@@ -1,13 +1,22 @@
 /*
- *  FILEE  :  Mul.c
+ *  File   :  Mul.c
  *  Date   :  Nov 17, 2024
  *  Author :  Mariam Hossam
  */
  
-#include "Mul.h"
+#include "configuration.h"
  
+#ifdef MUL
 /***************    Function Implementation    ***************/
 double multiply(float a, float b)
 {
 	return (a * b);
 }
+#else
+double multiply(float a, float b)
+{
+	printf("Purchase Multiplication Feature to Unlock!\n");
+	printf("Press CTRL + C to Exit The Program and Launch It Again\n");
+	while(1);
+}
+#endif
